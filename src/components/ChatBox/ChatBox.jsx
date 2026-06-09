63c76893db0b66898282f4422720829853930895
dhananjay1801 <dhananjaytailor18@gmail.com>
2026-06-09 17:22:24 +0530

    left and middle bars done

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './ChatBox.css'
+import assets from '../../assets/assets'
+
+const ChatBox = () => {
+  return (
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} />
+        <p>Richard Sanford <img src={assets.green_dot} className='dot'/></p>
+        <img src={assets.help_icon} className='help'/>
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 pm</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img src={assets.pic1} className='msg-img' alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 pm</p>
+          </div>
+        </div>
+
+        <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 pm</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message'/>
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default ChatBox
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-import assets from '../../assets/assets' 
+import assets from '../../assets/assets'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+
   return (
     <div className="login">
-      <img src={assets.logo_big} alt="" className='logo'/>
+      <img src={assets.logo_big} alt="" className='logo' />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input type="text" placeholder='Username' className="form-input" required/>
-        <input type="email" placeholder='Email Address' className="form-input" />
-        <input type="text" placeholder='Password' className="form-input" />
-        <button type='submit'>Sign Up</button>
+        <h2>{currentState}</h2>
+        {currentState == "Sign Up" ? <input type="text" placeholder='Username' className="form-input" required /> : null}
+        <input type="email" placeholder='Email Address' className="form-input" required />
+        <input type="text" placeholder='Password' className="form-input" required />
+        <button type='submit'>{currentState === "Sign Up" ? "Create Account" : "Log In"}</button>
         <div className="login-term">
-          <input type="checkbox"/>
+          <input type="checkbox" />
           <p>Agree to terms & use of privacy policy.</p>
         </div>
         <div className="login-forgot">
-          <p>Already have an account <span>click here</span></p>
+          {currentState === "Sign Up"
+            ? <p className='login-toggle'>Already have an account <span onClick={() => setCurrentState("Log In")}>Login here</span></p>
+            : <p className='login-toggle'>Create an account <span onClick={() => setCurrentState("Sign Up")}>click here</span></p>
+          }
         </div>
       </form>
     </div>
